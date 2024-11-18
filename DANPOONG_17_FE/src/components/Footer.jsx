98374a1126616f3c1bb07b8f8d5217ed977b1d14
E2YunJeong <dbsdbs3440@gmail.com>
2024-11-19 05:06:37 +0900

    🎨 Feat: "Footer 이미지 색상 변화 수정"
    아이콘 클릭 시 아이콘 이미지 색상도 변경되도록 수정

--- DANPOONG_17_FE/src/components/Footer.jsx
+++ DANPOONG_17_FE/src/components/Footer.jsx
@@ -5,30 +5,37 @@ import homeIcon from '../assets/navi_bar/home.png';
 import recipeIcon from '../assets/navi_bar/recipe.png';
 import commuIcon from '../assets/navi_bar/community.png';
 import myIcon from '../assets/navi_bar/my.png';
+import homeIconOn from '../assets/navi_bar/home_on.png';
+import recipeIconOn from '../assets/navi_bar/recipe_on.png';
+import commuIconOn from '../assets/navi_bar/community_on.png';
+import myIconOn from '../assets/navi_bar/my_on.png';
 
 function Footer() {
   const location = useLocation();
   const navigate = useNavigate();
 
   const menuItems = [
-    { name: '홈', path: '/', icon: homeIcon },
-    { name: '레시피', path: '/recipe', icon: recipeIcon },
-    { name: '커뮤니티', path: '/community', icon: commuIcon },
-    { name: '마이', path: '/my', icon: myIcon },
+    { name: '홈', path: '/', icon: homeIcon, activeIcon: homeIconOn },
+    { name: '레시피', path: '/recipe', icon: recipeIcon, activeIcon: recipeIconOn },
+    { name: '커뮤니티', path: '/community', icon: commuIcon, activeIcon: commuIconOn },
+    { name: '마이', path: '/my', icon: myIcon, activeIcon: myIconOn },
   ];
 
   return (
     <footer className="footer">
-      {menuItems.map((item) => (
-        <div
-          key={item.name}
-          className={`footer-icon ${location.pathname === item.path ? 'active' : ''}`}
-          onClick={() => navigate(item.path)}
-        >
-          <img src={item.icon} alt={item.name} />
-          <p>{item.name}</p>
-        </div>
-      ))}
+      {menuItems.map((item) => {
+        const isActive = location.pathname === item.path;
+        return (
+            <div
+                key={item.name}
+                className={`footer-icon ${isActive ? 'active' : ''}`}
+                onClick={() => navigate(item.path)}
+            >
+                <img src={isActive ? item.activeIcon : item.icon} alt={item.name} />
+                <p>{item.name}</p>
+            </div>
+        );
+    })}
     </footer>
   );
 }
