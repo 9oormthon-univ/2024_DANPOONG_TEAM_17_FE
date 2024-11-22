a7410f07269de9ed0f7be37de167bdd78fbcfd01
E2YunJeong <dbsdbs3440@gmail.com>
2024-11-22 18:23:11 +0900

    ✨ Feat: "마이페이지 추가"

--- DANPOONG_17_FE/src/pages/MyPage.jsx
+++ DANPOONG_17_FE/src/pages/MyPage.jsx
@@ -1,8 +1,56 @@
-export function My() {
-    return (
-      <div>
-        <h1>My Page</h1>
-        <p>마이 페이지</p>
+import '../styles/MyPage.css';
+
+import alertIcon from '../assets/myPage/alert.png';
+import userIcon from '../assets/myPage/user.png';
+import backIcon from '../assets/myPage/back.png';
+import bookmarkIcon from '../assets/myPage/북마크.png';
+import settingIcon from '../assets/myPage/setting.png';
+
+
+export const My = () => {
+  return (
+    <div className="mypage">
+      {/* Header Section */}
+      <div className="user-section">
+        <img src={userIcon} alt="User Icon" className="user-icon" />
+        <p className="user-name">미르미</p>
       </div>
-    );
-}
+
+      {/* Icon Section */}
+      <div className="icon-section">
+        <div className="icon-item">
+          <img src={bookmarkIcon} alt="Bookmark Icon" className="icon-image" />
+          <p>스크랩북</p>
+        </div>
+        <div className="icon-item">
+          <img
+            src={alertIcon}
+            alt="Notification Icon"
+            className="icon-image"
+          />
+          <p>알림</p>
+        </div>
+        <div className="icon-item">
+          <img src={settingIcon} alt="Settings Icon" className="icon-image" />
+          <p>설정</p>
+        </div>
+      </div>
+
+      {/* List Section */}
+      <div className="list-section">
+        <div className="list-item">
+          <p>내가 쓴 글</p>
+          <img src={backIcon} alt="Arrow Icon" className="arrow-icon" />
+        </div>
+        <div className="list-item">
+          <p>신청한 쿠킹 클래스</p>
+          <img src={backIcon} alt="Arrow Icon" className="arrow-icon" />
+        </div>
+        <div className="list-item">
+          <p>내 채팅방</p>
+          <img src={backIcon} alt="Arrow Icon" className="arrow-icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
