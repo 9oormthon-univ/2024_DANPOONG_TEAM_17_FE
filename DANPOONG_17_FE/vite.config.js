243f7ba25d542edc3ea940dd5648192825457116
E2YunJeong <dbsdbs3440@gmail.com>
2024-11-23 08:53:30 +0900

    🎨 Feat: "api 설정"

--- DANPOONG_17_FE/vite.config.js
+++ DANPOONG_17_FE/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://52.78.33.37:8080', // 백엔드 API 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // "/api" 제거
+      },
+    },
+  },
+});
