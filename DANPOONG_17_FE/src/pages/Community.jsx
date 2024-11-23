b0e834c3ec48a447c70a69b9433aa6a203f4a512
E2YunJeong <dbsdbs3440@gmail.com>
2024-11-23 12:20:30 +0900

    🎨 Feat: "커뮤니티 페이지 이미지 변경"

--- DANPOONG_17_FE/src/pages/Community.jsx
+++ DANPOONG_17_FE/src/pages/Community.jsx
@@ -1,6 +1,14 @@
 import '../styles/CommunityPage.css';
 import Header from '../components/Header';
-import dummy_img from '../assets/dummy/계란찜.jpg';
+import class1 from '../assets/community/class1.jpg';
+import class2 from '../assets/community/class2.jpg';
+import class3 from '../assets/community/class3.jpg';
+import together1 from '../assets/community/together1.jpg';
+import together2 from '../assets/community/together2.jpg';
+import together3 from '../assets/community/together3.jpg';
+import popular1 from '../assets/community/popular1.jpg';
+import popular2 from '../assets/community/popular2.jpg';
+import popular3 from '../assets/community/popular3.jpg';
 
 export const Community = () => {
   const sections = [
@@ -8,19 +16,19 @@ export const Community = () => {
       title: '쿠킹 클래스',
       description:
         '세계 각국의 요리를 배울 수 있는 쿠킹클래스! 다양한 문화를 직접 체험하며 새로운 요리 스킬을 익혀보세요.',
-      images: [dummy_img, dummy_img, dummy_img], // 이미지 파일명
+      images: [class1, class2, class3], // 이미지 파일명
     },
     {
       title: '함께 요리하기',
       description:
         '같이 요리하며 새로운 친구를 만나보세요! 다양한 국적의 사람들과 함께 요리하며 문화와 맛의 교류를 즐길 수 있습니다.',
-      images: [dummy_img, dummy_img, dummy_img],
+      images: [together1, together2, together3],
     },
     {
       title: '맛집 탐방하기',
       description:
         '현지인이 추천하는 맛집을 탐방하며 진정한 로컬 맛을 느껴보세요. 함께 음식의 매력을 나누며 새로운 추억을 만들어보세요.',
-      images: [dummy_img, dummy_img, dummy_img],
+      images: [popular1, popular2, popular3],
     },
   ];
 
